5822eb3b1a26c4c3de45fe7a42cdbc24052dd8de
Pedro Vieira <pedro.vieira@polarising.com>
2017-12-06 16:00:19 +0000

    Dependency Injection using Spring Framework

--- src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -1,16 +1,20 @@
 package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    String sayHello(){
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 }
